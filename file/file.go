3445d85451b8ccd2d639518c14b709d18ab4e13e
arun <arunkumar.paramasivam@ioof.com.au>
2020-04-19 22:10:31 +1000

    copy array of files to specifiied location

--- file/file.go
+++ file/file.go
@@ -1,1 +1,40 @@
 package file
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func Copy(files []string, location string) error {
+	os.MkdirAll(location, 0777)
+	for _, file := range files {
+		fileHandle, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := fileHandle.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		_, fileName := filepath.Split(file)
+		targetFile := filepath.Join(location, fileName)
+		destinationFileHandle, err := os.OpenFile(targetFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0777)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := destinationFileHandle.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		_, err = io.Copy(destinationFileHandle, fileHandle)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- file/file_test.go
+++ file/file_test.go
@@ -1,1 +1,31 @@
 package file
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestCopy(t *testing.T) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf(" Problem in finding current working directory")
+	}
+	copyPath, _ := filepath.Abs(pwd + "/../test/copy")
+	testcsv := filepath.Join(copyPath, "test.csv")
+	music := filepath.Join(copyPath, "Music")
+
+
+	Copy([]string{testcsv}, music)
+
+	stat, err := os.Stat(filepath.Join(music, "test.csv"))
+	if err != nil || stat.Size() != 5 {
+		t.Errorf("expected '%v' but got '%v'", stat.Size(), 5)
+	}
+
+	t.Cleanup(func() {
+		if err == nil {
+			os.RemoveAll(music)
+		}
+	})
+}
